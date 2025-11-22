fef6daeda79c3f60fee4b8b7cd29ca96a72048be
Jose Nogales <148090797+jnogalesg@users.noreply.github.com>
2025-11-22 21:15:14 +0100

    Add 'rest_framework' and 'comunidades' to INSTALLED_APPS

--- mymicroservice/mymicroservice/settings.py
+++ mymicroservice/mymicroservice/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # <- para las APIs
+    'comunidades',     # <- aplicación principal
 ]
 
 MIDDLEWARE = [
