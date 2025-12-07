8a5b647e5c35a9c7681f807f8f52fad49c0c426f
Jose Nogales <jnogalesye@alumnos.unex.es>
2025-12-07 02:35:26 +0100

    fixed/Ocultada Secret Key de Django

--- mymicroservice/mymicroservice/settings.py
+++ mymicroservice/mymicroservice/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8p2#qhd&14@$bkrso-c1hcwr%ji1n-9%0-pr&l@gycei+7c!xz'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
