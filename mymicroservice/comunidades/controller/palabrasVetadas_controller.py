c9e2265e8c08e0bd5a2dab0c1acd0af2f2f68af2
Jose Nogales <jnogalesye@alumnos.unex.es>
2025-12-07 03:24:31 +0100

    fixed/agregada cte. en palabrasVetadas para evitar duplicidad

--- mymicroservice/comunidades/controller/palabrasVetadas_controller.py
+++ mymicroservice/comunidades/controller/palabrasVetadas_controller.py
@@ -7,90 +7,92 @@ import dataclasses
 
 class PalabrasVetadasController(APIView):
 
-    def get(self, request, idComunidad):
+    errIdCom = "Falta errIdComunidad en la URL" # Constante para mensajes de error
+    
+    def get(self, request, errIdComunidad):
         """
-        GET /comunidad/<idComunidad>/palabras-vetadas/
+        GET /comunidad/<errIdComunidad>/palabras-vetadas/
         Obtiene la lista de palabras vetadas para una comunidad específica.
         """
-        if not idComunidad: # Comprobamos que se ha pasado idComunidad en la URL
-             return Response({"error": "Falta idComunidad en la URL"}, status=status.HTTP_400_BAD_REQUEST)
+        if not errIdComunidad: # Comprobamos que se ha pasado errIdComunidad en la URL
+             return Response({"error": self.errIdCom}, status=status.HTTP_400_BAD_REQUEST)
          
         try:    # Verificamos que la comunidad existe, si no, salta una excepción
-            Comunidad.objects.get(idComunidad=idComunidad)
+            Comunidad.objects.get(errIdComunidad=errIdComunidad)
         except Comunidad.DoesNotExist:
-            return Response({"error": f"Comunidad con id {idComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": f"Comunidad con id {errIdComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
 
         try:
-            dto = PalabrasVetadasDAO.get_palabras_vetadas(idComunidad)
+            dto = PalabrasVetadasDAO.get_palabras_vetadas(errIdComunidad)
             return Response(dataclasses.asdict(dto), status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
 
-    def post(self, request, idComunidad):
+    def post(self, request, errIdComunidad):
         """ 
-        POST /comunidad/<idComunidad>/palabras-vetadas/
+        POST /comunidad/<errIdComunidad>/palabras-vetadas/
         Añade nuevas palabras vetadas a la comunidad.
         """
-        if not idComunidad: # Comprobamos que se ha pasado idComunidad en la URL
-             return Response({"error": "Falta idComunidad en la URL"}, status=status.HTTP_400_BAD_REQUEST)
+        if not errIdComunidad: # Comprobamos que se ha pasado errIdComunidad en la URL
+             return Response({"error": self.errIdCom}, status=status.HTTP_400_BAD_REQUEST)
          
         try:    # Verificamos que la comunidad existe, si no, salta una excepción
-            Comunidad.objects.get(idComunidad=idComunidad)
+            Comunidad.objects.get(errIdComunidad=errIdComunidad)
         except Comunidad.DoesNotExist:
-            return Response({"error": f"Comunidad con id {idComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": f"Comunidad con id {errIdComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
                  
         try:
             nuevas_palabras = request.data.get('palabras', [])
             if not isinstance(nuevas_palabras, list):
                  return Response({"error": "Se espera una lista en el campo 'palabras'"}, status=status.HTTP_400_BAD_REQUEST)
                  
-            dto = PalabrasVetadasDAO.add_palabras_vetadas(idComunidad, nuevas_palabras)
+            dto = PalabrasVetadasDAO.add_palabras_vetadas(errIdComunidad, nuevas_palabras)
             return Response(dataclasses.asdict(dto), status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, idComunidad):
+    def put(self, request, errIdComunidad):
         """
-        PUT /comunidad/<idComunidad>/palabras-vetadas/
+        PUT /comunidad/<errIdComunidad>/palabras-vetadas/
         Reemplaza toda la lista de palabras vetadas de la comunidad.
         """
-        if not idComunidad: # Comprobamos que se ha pasado idComunidad en la URL
-             return Response({"error": "Falta idComunidad en la URL"}, status=status.HTTP_400_BAD_REQUEST)
+        if not errIdComunidad: # Comprobamos que se ha pasado errIdComunidad en la URL
+             return Response({"error": self.errIdCom}, status=status.HTTP_400_BAD_REQUEST)
          
         try:    # Verificamos que la comunidad existe, si no, salta una excepción
-            Comunidad.objects.get(idComunidad=idComunidad)
+            Comunidad.objects.get(errIdComunidad=errIdComunidad)
         except Comunidad.DoesNotExist:
-            return Response({"error": f"Comunidad con id {idComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": f"Comunidad con id {errIdComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
 
         try:
             nueva_lista = request.data.get('palabras', [])
             if not isinstance(nueva_lista, list):
                  return Response({"error": "Se espera una lista en el campo 'palabras'"}, status=status.HTTP_400_BAD_REQUEST)
 
-            dto = PalabrasVetadasDAO.modificar_palabras_vetadas(idComunidad, nueva_lista)
+            dto = PalabrasVetadasDAO.modificar_palabras_vetadas(errIdComunidad, nueva_lista)
             return Response(dataclasses.asdict(dto), status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, idComunidad):
+    def delete(self, request, errIdComunidad):
         """
-        DELETE /comunidad/<idComunidad>/palabras-vetadas/
+        DELETE /comunidad/<errIdComunidad>/palabras-vetadas/
         Elimina palabras específicas de la lista de palabras vetadas de la comunidad.
         """
-        if not idComunidad: # Comprobamos que se ha pasado idComunidad en la URL
-             return Response({"error": "Falta idComunidad en la URL"}, status=status.HTTP_400_BAD_REQUEST)
+        if not errIdComunidad: # Comprobamos que se ha pasado errIdComunidad en la URL
+             return Response({"error": self.errIdCom}, status=status.HTTP_400_BAD_REQUEST)
          
         try:    # Verificamos que la comunidad existe, si no, salta una excepción
-            Comunidad.objects.get(idComunidad=idComunidad)
+            Comunidad.objects.get(errIdComunidad=errIdComunidad)
         except Comunidad.DoesNotExist:
-            return Response({"error": f"Comunidad con id {idComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": f"Comunidad con id {errIdComunidad} no encontrada."}, status=status.HTTP_404_NOT_FOUND)
 
         try:
             a_borrar = request.data.get('palabras', [])
             if not isinstance(a_borrar, list):
                  return Response({"error": "Se espera una lista en el campo 'palabras'"}, status=status.HTTP_400_BAD_REQUEST)
 
-            dto = PalabrasVetadasDAO.eliminar_palabras_vetadas(idComunidad, a_borrar)
+            dto = PalabrasVetadasDAO.eliminar_palabras_vetadas(errIdComunidad, a_borrar)
             return Response(dataclasses.asdict(dto), status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
